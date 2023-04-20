10c52304acd0c1045c6cd6431f9d74a51038de01
Evgeni Pavlov <evgeni.pavlov@toptal.com>
2023-04-20 22:45:00 +0300

    Add animated message at the end

--- fire.sh
+++ fire.sh
@@ -5,6 +5,22 @@ function usage() {
   exit 1
 }
 
+function animate_string() {
+  local string="$1"
+  
+  for (( i=1; i<=${#string}; i++ )); do
+    local char="${string:i-1:1}"
+    printf "\r%s" "${string:0:i}"
+    tput setaf 1; printf "🔥"; tput sgr0
+    sleep 0.03
+    printf "\r"; tput setaf 1; printf "%s" "${string:0:i}"; tput sgr0
+    sleep 0.03
+    tput setaf 1; printf "%s" "$char"; tput sgr0
+  done
+  
+  printf "\r"; tput setaf 1; printf "%s" "$string"; tput sgr0; printf "\n"
+}
+
 [[ $# -eq 0 ]] && usage
 
 killed_count=0
@@ -38,8 +54,11 @@ for ((i=0; i<killed_count; i++)); do
 done
 echo ""
 
-if [ $killed_count -gt 1 ]; then
-  echo "$killed_count processes are no more."
-else
-  echo "The process is no more."
+if [ $killed_count -gt 0 ]; then
+  sleep 0.2
+  if [ $killed_count -gt 1 ]; then
+    animate_string " $killed_count processes are no more. "
+  else
+    animate_string " The process is no more. "
+  fi
 fi
