14aae2fdccb9a65d4f39f8877dcb3e843a6b2524
Evgeni Pavlov <evgeni.pavlov@toptal.com>
2023-04-20 22:22:54 +0300

    Add emojis to output

--- fire.sh
+++ fire.sh
@@ -1,39 +1,45 @@
 #!/bin/bash
 
-# Check if port number or process name is supplied as argument
-if [ $# -eq 0 ]; then
+function usage() {
   echo "Usage: $0 <port_number or process_name> [<port_number or process_name> ...]"
   exit 1
-fi
+}
 
-# Loop through all arguments provided
-for arg in "$@"
-do
-  # Check if argument is a valid port number
+[[ $# -eq 0 ]] && usage
+
+killed_count=0
+
+for arg in "$@"; do
   if [[ "$arg" =~ ^[0-9]+$ ]]; then
-    # Find process ID listening on specified port
-    pid=$(lsof -ti:$arg)
-
-    # Check if process ID was found
-    if [ -z "$pid" ]; then
-      echo "No process found listening on port $arg"
+    pids=$(lsof -ti:$arg)
+    if [ -z "$pids" ]; then
+      echo "No process found listening on port $arg 🧯"
     else
-      # Kill the process listening on specified port
-      kill -9 $pid
-      echo "Process listening on port $arg has been killed"
+      for pid in $pids; do
+        kill -9 $pid &>/dev/null
+        ((killed_count++))
+      done
     fi
-
   else
-    # Find process ID with specified name
-    pid=$(pgrep -f "$arg")
-
-    # Check if process ID was found
-    if [ -z "$pid" ]; then
-      echo "No process found with name $arg"
+    pids=$(pgrep -f "$arg")
+    if [ -z "$pids" ]; then
+      echo "No process found with name $arg 🧯"
     else
-      # Kill the process with specified name
-      kill -9 $pid
-      echo "Process with name $arg has been killed"
+      for pid in $pids; do
+        kill -9 $pid &>/dev/null
+        ((killed_count++))
+      done
     fi
   fi
 done
+
+for ((i=0; i<killed_count; i++)); do
+  echo -n "🔥"
+done
+echo ""
+
+if [ $killed_count -gt 1 ]; then
+  echo "$killed_count processes are no more."
+else
+  echo "The process is no more."
+fi
